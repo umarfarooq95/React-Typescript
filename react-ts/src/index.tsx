c85add62eeefcc6dd7d8502d61b0bda6e9ed0fe4
umarfarooq95 <mohamedumar931@gmail.com>
2019-12-26 10:03:25 +0530

    first commit

---
+++ react-ts/src/index.tsx
@@ -0,0 +1,8 @@
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+
+const App: React.SFC = () => {
+  return <h1>My React and Typescript App!</h1>;
+};
+
+ReactDOM.render(<App/>, document.getElementById("root"))
